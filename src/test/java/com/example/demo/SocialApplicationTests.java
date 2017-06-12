87dc6a89f2b833f98d27555526d9a1cfb06523ac
Terry Bisiar <t.bisiar@auckland.ac.nz>
2017-06-12 22:57:53 +1200

    Second step working, now have to click link to login, reads name back from Facebook

--- src/test/java/com/example/demo/SocialApplicationTests.java
+++ src/test/java/com/example/demo/SocialApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SimpleApplicationTests {
+public class SocialApplicationTests {
 
 	@Test
 	public void contextLoads() {
